8d93d90dd0652dff7ab1f35c74f3f94da6950b8e
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-10-30 09:36:36 +0530

    establish intial socket connection with frontend

--- src/server.js
+++ src/server.js
@@ -1,21 +1,20 @@
 const express = require('express');
-const { createServer } = require('node:http');
-const { join } = require('node:path');
-const { Server } = require('socket.io');
-
 const app = express();
-const server = createServer(app);
-const io = new Server(server);
+const http = require('http');
+const { Server } = require("socket.io");
 
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'index.html'));
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {origin:"http://localhost:5173", methods: ["GET", "POST"]},
 });
 
+io.on("connection", (socket) => {
+    console.log(`a user connected ${socket.id}`);
+    
+    socket.on("send_message", (data) => {
+        console.log(data);
+    //   socket.broadcast.emit("receive_message", data);
+    });
+  });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-});
-
-server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
-});
+server.listen(3000, () => { console.log("listening on 3000"); });
