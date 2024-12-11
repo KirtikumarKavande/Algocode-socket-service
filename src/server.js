e09c6a7ae489d9e7095559bceb8f64f32d1b5e3e
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-12-11 21:28:35 +0530

    feat: add ping router for test

--- src/server.js
+++ src/server.js
@@ -19,6 +19,10 @@ const io = new Server(server, {
     cors: {origin:["http://localhost:5173","http://localhost:5000"], methods: ["GET", "POST"]},
 });
 app.use(express.json())
+
+app.get("/ping", (req, res) => {
+    res.send("pong");
+})
 io.on("connection", (socket) => {
     console.log(`a user connected ${socket.id}`);
     
