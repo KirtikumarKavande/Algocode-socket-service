dd00122b3a4279704622d4bf4ed3c2c705a69b2d
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-12-14 15:12:26 +0530

    refactor: update frontend url to avoid cors error

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ if(process.env.REDIS_HOST){
 
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: {origin:["http://localhost:5173","http://localhost:5000"], methods: ["GET", "POST"]},
+    cors: {origin:["http://localhost:5173","http://localhost:5000","https://algocode-phi.vercel.app"], methods: ["GET", "POST"]},
 });
 app.use(express.json())
 
